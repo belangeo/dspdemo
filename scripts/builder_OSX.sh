d7eb0ed1527b0ae3ec47652fa0e5e55dd51531ef
Olivier Belanger <belangeo@gmail.com>
2018-03-13 15:28:23 -0400

    Upgraded version number to 0.0.9.

--- scripts/builder_OSX.sh
+++ scripts/builder_OSX.sh
@@ -5,8 +5,8 @@
 # Olivier Belanger, 2018
 #####################################
 
-export DMG_DIR="DSPDemo 0.0.8"
-export DMG_NAME="DSPDemo_0.0.8.dmg"
+export DMG_DIR="DSPDemo 0.0.9"
+export DMG_NAME="DSPDemo_0.0.9.dmg"
 
 python3.6 setup.py py2app --plist=scripts/info.plist
 
